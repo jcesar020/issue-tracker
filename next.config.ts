bd5c36905b92d4f3c2f34654f13fd72038b63bc9
Cesar <cesar@MacBook-Pro-de-Mario.local>
2025-09-29 18:57:53 -0600

    Add config ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true, // 🚨 evita que el build falle por errores de ESLint
+  },
 };
 
 export default nextConfig;
