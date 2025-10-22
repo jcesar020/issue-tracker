251f87a4a5de2a4fe39a72d1cf53813a4336d84c
Cesar <cesar@MacBook-Pro-de-Mario.local>
2025-10-21 22:36:03 -0600

    fix validationSchema import module

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,4 +1,4 @@
-import z from "@/node_modules/zod/v4/classic/external.cjs";
+import { z } from "zod";
 
 export const createIssueSchema = z.object({
   title: z.string().min(1, 'Title is required').max(100),
